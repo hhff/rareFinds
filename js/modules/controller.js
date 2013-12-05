a4f66ec5e9065560cc9a865f744d5374f5ca3bb9
hhff <me@hughfrancis.me>
2013-12-05 17:31:16 +1100

    ripping

--- js/modules/controller.js
+++ js/modules/controller.js
@@ -20,6 +20,53 @@ define(['jquery'], function($){
 		})
 
 
+		//Email Validation
+		$('#email').on('keyup', function(e){
+			var $input = $(this),
+				formVal = $(this).val();
+			if (controller._validateEmail(formVal) == true){
+				validEmail = true;
+				$input.removeClass('invalid');
+				$input.addClass('valid');
+			}else{
+				validEmail = false;
+				$input.removeClass('valid');
+				$input.addClass('invalid');
+			}
+
+			if (!formVal){
+				$input.removeClass('valid');
+				$input.removeClass('invalid');
+			}
+
+			if (e.keyCode === 13) {
+				$('#js-emailForm').submit();
+			}
+
+		});
+
+		$('#email').on('focus', function(){
+			$(this).attr('placeholder', '')
+		})
+
+		$('#email').on('blur', function(){
+			$(this).attr('placeholder', 'Get Updates')
+		})
+
+		$('#emailSubmit').on('click', function(){
+			$('#js-emailForm').submit();
+		})
+
+		$('#js-emailForm').submit(function(e){
+			if (validEmail == true){
+				//submit
+			}else{
+				e.preventDefault();
+				//no submit
+			}
+		})
+
+
 	}
 
 	controller._navClick = function(context){
@@ -30,6 +77,12 @@ define(['jquery'], function($){
 		$featureImg.removeClass('active');
 		$featureImg.filter('#'+context).addClass('active');
 	}
+
+	controller._validateEmail = function(email){
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	}
+
 
 	return controller;
 });
